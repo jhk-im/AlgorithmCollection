6e481633f856e7c232a899fd38b4118123d8f2e8
jrooms <wanted0301@gmail.com>
2021-05-18 12:16:50 +0900

    feature: Create coding test - hash1

--- src/main/kotlin/hash/test1/HashCodingTest1.kt
+++ src/main/kotlin/hash/test1/HashCodingTest1.kt
@@ -8,20 +8,32 @@ class HashCodingTest1 {
         val map = HashMap<String, Int>()
 
         for (player : String in participant) {
+
+            /*map[player]?.let {
+                map[player] = it + 1
+            }
+
+            if (map[player] == null) {
+                map[player] = 1
+            }*/
+
             map[player] = map.getOrDefault(player, 0) + 1
         }
+        println(map)
 
         for (player : String in completion) {
             map[player]?.apply {
                 map[player] = this - 1
             }
         }
+        println(map)
 
         for (key : String in map.keys) {
             if (map[key] != 0) {
-                answer = key;
+                answer = key
             }
         }
+        println(map)
 
         return answer
     }
