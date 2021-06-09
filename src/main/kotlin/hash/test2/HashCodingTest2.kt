70620227649fdda8322c60f0e16ab144eea3ebcc
jrooms <wanted0301@gmail.com>
2021-06-10 02:28:47 +0900

    feature: Create coding test - hash2

--- src/main/kotlin/hash/test2/HashCodingTest2.kt
+++ src/main/kotlin/hash/test2/HashCodingTest2.kt
@@ -1,5 +1,19 @@
 package hash.test2
 
+
 class HashCodingTest2 {
 
+    fun solution(phoneBook: Array<String>): Boolean {
+        val hashMap = HashMap<String, String?>()
+        for (phone in phoneBook) {
+            hashMap[phone] = phone
+        }
+        for (key in hashMap.keys) {
+            for (i in key.indices) {
+                if (hashMap.containsKey(key.substring(0, i))) return false
+                if (hashMap[key.substring(0, i)] != null) return false
+            }
+        }
+        return true
+    }
 }
