a612da15d5ade17848539abc06268441a4679b3c
jhk-im <kjhun200@gmail.com>
2023-04-20 15:32:42 +0900

    feat: ndb/05_dynamic_programming/test2

---
+++ ndb/06_dynamic_programming/test2/main.dart
@@ -0,0 +1,36 @@
+import 'dart:io';
+import 'dart:math';
+
+void main(List<String> arguments) {
+  // 정수 n 입력
+  var input1 = stdin.readLineSync();
+  if (input1 != null) {
+    var n = int.parse(input1);
+
+    // 모든 식량정보 입력
+    var input2 = stdin.readLineSync()?.split(' ');
+    if (input2 != null) {
+      var array = List.filled(n + 1, 0);
+      var count = 0;
+      for (String s in input2) {
+        array[count] = int.parse(s);
+        count++;
+      }
+
+      // 계산된 결과를 저장하기 위한 DP 테이블
+      var d = List.filled(100, 0);
+      d[0] = array[0];
+      d[1] = max(array[0], array[1]);
+
+      // 다나믹 프로그래밍 바텀업
+      for (int i = 2; i < n + 1; i++) {
+        print("---");
+        print("i - > $i");
+        print("d[i-1] - > ${d[i - 1]}");
+        print("d[i-1]+array[i] - > ${d[i - 2] + array[i]}");
+        d[i] = max(d[i - 1], d[i - 2] + array[i]);
+        print("d[i] -> ${d[i]}");
+      }
+    }
+  }
+}
---
+++ ndb/06_dynamic_programming/test2/main.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        try {
+            // 정수 n 입력
+            String input = br.readLine();
+            int n = Integer.parseInt(input);
+
+            // 모든 식량정보 입력
+            BufferedReader br2 = new BufferedReader(new InputStreamReader(System.in));
+            String[] input2 = br2.readLine().split(" ");
+            int[] array = new int[n + 1];
+            int count = 0;
+            for (String s : input2) {
+                array[count] = Integer.parseInt(s);
+                count++;
+            }
+
+            // 앞서 계산된 결과 저장하기 위한 DP 테이블
+            int[] d = new int[100];
+            d[0] = array[0];
+            d[1] = Math.max(array[0], array[1]);
+
+            // 다나믹 프로그래밍 바텀업
+            for (int i = 2; i < n + 1; i++){
+                System.out.println("---");
+                System.out.println("i -> " + i);
+                System.out.println("d[i - 1] -> " + d[i - 1]);
+                int temp = d[i - 2] + array[i];
+                System.out.println("d[i - 1] + array[i] -> " + temp);
+                d[i] = Math.max(d[i - 1], temp);
+                System.out.println("d[i] -> " + d[i]);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
